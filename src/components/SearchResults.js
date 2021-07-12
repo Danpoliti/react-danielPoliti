90136f7cefa9504d58c6fad4019cc43dab1e72bb
Danpoliti <danielcpoliti@yahoo.com.br>
2021-07-11 21:49:00 -0400

    Added basic pagination

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,19 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ProductRow from 'components/ProductRow'
+import { Pagination } from 'antd'
+import 'antd/dist/antd.css'
 
 const SearchResults = ({result}) => {
 
-  const theProducts = result.slice(0,3).map((product) => <ProductRow key={product.id} data={product} />)
+  const [currPage, setCurrPage] = useState(1)
+  const pageSize = 9
+
+  const startRow = (currPage - 1) * pageSize
+  const endRow = startRow + pageSize
+  const totalPages = Math.ceil(result.length / pageSize)
+
+  const theProducts = result.slice(startRow,endRow).map((product) => <ProductRow key={product.id} data={product} />)
   // Pagination is next
 
   // How many are in the original set (total that meet the criteria)?
   // How many to display per page?
   // What page are we currently showing?
 
+  const updatePage = (page) => {
+    if (page < 0) 
+      setCurrPage(1)
+    else if (page > totalPages) 
+      setCurrPage(totalPages)
+    else 
+      setCurrPage(page)
+  }
 
   return (
       <>
         {theProducts}
+        <div className="pagination">
+        <p style={{backgroundColor: `purple`} }>
+        <button onClick={()=> updatePage(currPage-1)} disabled={(currPage === 1 )?`disabled`:``}>prev</button>
+        Showing {(theProducts.length === 1) ? `product` : `products`} {startRow + 1} to {Math.min(endRow, result.length)} of {result.length}
+        <button onClick={()=> updatePage(currPage+1)} disabled={(currPage === totalPages )?`disabled`:``} >next</button>
+      </p>
+      
+        <Pagination defaultCurrent={currPage} total={result.length} defaultPageSize={pageSize} onChange={(page) => setCurrPage(page)} />
+        </div>
     </>
   )
 }
