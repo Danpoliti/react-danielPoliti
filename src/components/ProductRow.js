c21420f60e64e3e3d6133213abf616cfa8395d2c
Danpoliti <danielcpoliti@yahoo.com.br>
2021-08-19 12:05:24 -0400

    added favorite functionality

--- src/components/ProductRow.js
+++ src/components/ProductRow.js
@@ -1,62 +1,72 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import Image from './Image.js'
+import firebase from 'utils/firebase'
+
 // import noImg from 'img/student.png'
 
-const ProductRow = ({data}) => {
-    const {id, name, category, regularPrice, price, details , img, color, type } = data
+const ProductRow = ({ data }) => {
+    const { id, name, category, regularPrice, price, details, img, color, type } = data
 
-    console.log("Image file:---", img[0])
+ 
+    const [fav, setFav] = useState(false);
+    const db = firebase.firestore()
 
+
+const addToFavorite = () => {
+    console.log(fav);
+    db.collection("productsData")
+      .doc(id)
+      .update({
+        fav: !fav,
+      })
+      .then(() => {
+        setFav(!fav);
+        console.log("'Favorite' updated!");
+      })
+      .catch((error) => {
+        console.error("Error writing document: ", error);
+      });
+
+}
     return (
         <section className="results">
 
 
-        <article className="product">
-<div className="product-grid">
-                <Image src={img[0]} alt="Product Image" className="product-img" id="bigimage" />
-                <Image src={img[1]} alt="Product Image" className="thumb-img" />
-                <Image src={img[2]} alt="Product Image" className="thumb2-img" />
-                <Image src={img[3]} alt="Product Image" className="thumb3-img" />
-            </div>
-            <h3><Link to={`/product/${id}`}>{name} - {color}</Link></h3>
-            <header className="product-header">
+            <article className="product">
+                <div className="product-grid">
+                    <Image src={img[0]} alt="Product Image" className="product-img" id="bigimage" />
+                    <Image src={img[1]} alt="Product Image" className="thumb-img" />
+                    <Image src={img[2]} alt="Product Image" className="thumb2-img" />
+                    <Image src={img[3]} alt="Product Image" className="thumb3-img" />
+                </div>
+                <h3><Link to={`/product/${id}`}>{name} - {color}</Link></h3>
+                <header className="product-header">
 
-                <data value="179" className="product-price"><del>{regularPrice}</del><br /><ins>{price}</ins>
-                </data>
-                <p className="details">{details}
-                <a href="https://en.wikipedia.org/wiki/Fender_Stratocaster#:~:text=The%20archetypical%20Stratocaster%20is%20a,made%20from%20alder%20or%20ash.&text=Most%20Stratocasters%20have%20three%20single,control%20and%20two%20tone%20controls.">see more</a></p>
-                <div className="product-rating">
-                <h4>Rating</h4>
-                    <span className="material-icons">star</span><span className="material-icons">star</span><span
-                    className="material-icons">star</span><span className="material-icons">star</span><span
-                    className="material-icons">star_half</span>
-                </div>
-                <form className="product-filter">
-                <fieldset>
-                    <legend>Colours</legend>
-                    <ul>
-                    <li><label><input type="radio" name="colour" value="r" /> Red</label></li>
-                    <li><label><input type="radio" name="colour" value="w" /> White</label></li>
-                    <li><label><input type="radio" name="colour" value="b" /> Black</label></li>
-                    </ul>
-                </fieldset>
-                <fieldset>
-                    <legend>Type</legend>
-                    <ol>
-                    <li><label><input type="radio" name="size" value="rhanded" /> RH</label></li>
-                    <li><label><input type="radio" name="size" value="lhanded" /> LH</label></li>
-                    </ol>
-                </fieldset>
-                </form>
-                <div className="add-cart">
-                <button type="button" className="add-cart"><span className="material-icons">shopping_cart</span> Add to Cart</button>
-                <button type="button" className="product-favorite"><span className="material-icons">favorite</span></button>
-                </div>
-            </header>
+                    <data value="179" className="product-price"><del>${regularPrice}</del><br /><ins>${price},00</ins>
+                    </data>
+                    <p className="details">{details}
+                        <a href="https://en.wikipedia.org/wiki/Fender_Stratocaster#:~:text=The%20archetypical%20Stratocaster%20is%20a,made%20from%20alder%20or%20ash.&text=Most%20Stratocasters%20have%20three%20single,control%20and%20two%20tone%20controls.">see more</a></p>
+                    <div className="product-rating">
+                        <h4>Rating</h4>
+                        <span className="material-icons">star</span><span className="material-icons">star</span><span
+                            className="material-icons">star</span><span className="material-icons">star</span><span
+                                className="material-icons">star_half</span>
+                    </div>
+
+                    <div className="add-cart">
+                        <button type="button" className="add-cart"><span className="material-icons">shopping_cart</span> Add to Cart</button>
+                       {fav ? (
+                        <button type="button" className="product-favorite" onClick={addToFavorite}><span className="material-icons">favorite</span></button>
+                        ) : ( <button type="button" className="product-favorite" onClick={addToFavorite}><span className="material-icons">favorite_border</span></button>)
+                       }
+                        </div>
+                </header>
             </article>
-            </section>
-  )
+        </section>
+    )
+
+
 }
 
 export default ProductRow
---
+++ src/components/SingleProductRow.js
@@ -0,0 +1,81 @@
+import React, { useContext, useState } from 'react'
+import Image from './Image.js'
+import UserContext from 'contexts/user'
+import { Link } from 'react-router-dom'
+import firebase from 'utils/firebase'
+
+// import 'css/product'
+
+
+
+const SingleProductRow = ({ data }) => {
+  const { id, name, category, regularPrice, price, details, img, color, type } = data
+
+  const [fav, setFav] = useState(false);
+  const db = firebase.firestore()
+
+
+  const addToFavorite = () => {
+      console.log(fav);
+      db.collection("productsData")
+        .doc(id)
+        .update({
+          fav: !fav,
+        })
+        .then(() => {
+          setFav(!fav);
+          console.log("'Favorite' updated!");
+        })
+        .catch((error) => {
+          console.error("Error writing document: ", error);
+        });
+  
+  }
+  const cartProduct = useContext(UserContext)
+  //   const handleAddCart = cartProduct.handleAddCart
+  //   const handleAddFav = cartProduct.handleAddFav
+
+
+  return (
+
+    <section className="single-results">
+      <h3><Link to={`/`}>BACK TO RESULT PAGE</Link></h3>
+
+
+      <article className="product">
+        <div className="product-grid">
+          <Image src={img[0]} alt="Product Image" className="product-img" id="bigimage" />
+          <Image src={img[1]} alt="Product Image" className="thumb-img" />
+          <Image src={img[2]} alt="Product Image" className="thumb2-img" />
+          <Image src={img[3]} alt="Product Image" className="thumb3-img" />
+        </div>
+        <h3>{name} - {color}</h3>
+        <header className="product-header">
+
+          <data value="179" className="product-price"><del>{regularPrice}</del><br /><ins>{price}</ins>
+          </data>
+          <p className="details">{details}
+            <a href="https://en.wikipedia.org/wiki/Fender_Stratocaster#:~:text=The%20archetypical%20Stratocaster%20is%20a,made%20from%20alder%20or%20ash.&text=Most%20Stratocasters%20have%20three%20single,control%20and%20two%20tone%20controls.">see more</a></p>
+          <div className="product-rating">
+            <h4>Rating</h4>
+            <span className="material-icons">star</span><span className="material-icons">star</span><span
+              className="material-icons">star</span><span className="material-icons">star</span><span
+                className="material-icons">star_half</span>
+          </div>
+
+          <div className="add-cart">
+            <button type="button" className="add-cart"><span className="material-icons">shopping_cart</span> Add to Cart</button>
+            {fav ? (
+              <button type="button" className="product-favorite" onClick={addToFavorite}><span className="material-icons">favorite</span></button>
+            ) : (<button type="button" className="product-favorite" onClick={addToFavorite}><span className="material-icons">favorite_border</span></button>)
+            }        </div>
+        </header>
+      </article>
+    </section>
+
+
+
+  )
+}
+
+export default SingleProductRow
