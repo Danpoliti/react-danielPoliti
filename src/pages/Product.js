23c8b1ee0f6109f9c1f5f75acbf64df92b5b9575
Danpoliti <danielcpoliti@yahoo.com.br>
2021-08-19 12:08:15 -0400

    single product page

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -2,11 +2,23 @@ import React, {useContext} from 'react'
 import { useParams } from 'react-router-dom'
 import Layout from 'components/Layout'
 import UserContext, {useUpdateUser} from 'contexts/user'
+import SingleProductRow from 'components/SingleProductRow'
 
 
 const Product = () => {
 
     const {slug} = useParams()
+  //console.log(slug)
+
+  const newItem = useContext(UserContext)
+//   const cartArr = newProduct.userCart
+//   const favArr = newProduct.userFav
+  
+  const productId = newItem.updateProduct(slug)
+  const singleProduct = productId.map(prod => <SingleProductRow key={prod.id} data={prod} />)
+  
+
+
     const user = useContext(UserContext)
 
     console.log(user.data)
@@ -15,7 +27,8 @@ const Product = () => {
     return (
         <Layout>
             <header>
-                <h1>Product {slug}</h1>
+                {/* <h1>Product {slug}</h1> */}
+                {singleProduct}
             </header>
         </Layout>
     )
